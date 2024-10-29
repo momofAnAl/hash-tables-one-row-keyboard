9c190bae96a87fd5342bcdeb8587b39e59e688d2
anhtran <anhtr077@gmail.com>
2024-10-28 21:52:59 -0700

    typing_type function

--- activity/activity.py
+++ activity/activity.py
@@ -1,6 +1,30 @@
 def typing_time(layout, word):
-    pass
+    keyboards = {char:pos for pos, char in enumerate(layout)}
     
-# skipping error handling
-# empty keys? empty word? repeated letters in keys?
+    
+    location = 0
+    total_time = 0
+    for letter in word:
+        next_location = keyboards[letter] 
+        total_time += abs(next_location - location)
+        location = next_location
+        
+    return total_time      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    
+
 
